25f462d0cf06cc28c62196bf8d0844feaacb316f
SukeerthiNaru <sukeerthinaru@gmail.com>
2025-03-06 13:09:02 +0530

    Add files via upload

---
+++ 6-3-25/task.js
@@ -0,0 +1,64 @@
+var arr=[1,2,3,4,5] //1st
+console.log(arr)
+
+//2nd
+var arr=[1,2,3,4,5]
+console.log(arr[3])
+
+//3rd
+var arr=[1,2,3,4,5]
+arr[1]=5
+console.log(arr)
+
+//4th ques
+function sum(){
+    var arr=[1,2,3,4,5]
+    return arr.length
+}
+console.log(sum())
+
+//5 ques
+var arr=[1,2,3,4,5]
+arr.push(6)
+console.log(arr)
+
+//6th ques
+var arr=[1,2,3,4,5]
+arr.pop()
+console.log(arr)
+
+//7th
+var arr=[1,2,3,4,5]
+for(i=0;i<arr.length;i++){
+console.log(arr[i])
+}
+
+//8th ques
+var arr=[1,2,3,4,5]
+b=6
+for(i=0;i<arr.length;i++){
+    if(b==arr[i]){
+        console.log('yes it is present in array')
+    }else
+    console.log('not present')}
+
+    //9th ques
+    var arr=[1,2,3,4,5]
+
+    var a=arr
+    console.log(arr)
+
+    //10 th ques
+    var arr=[1,2,3,4,5]
+    console.log(arr.join())
+
+
+
+
+
+
+
+
+
+
+
